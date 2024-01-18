0825e792b71318b3fe680f4ae6e0ee53ea15e8f6
adibmansuri511 <adibmansuri.developer@gmail.com>
2024-01-18 17:29:24 +0530

    Update all the files.

---
+++ assets/js/main.js
@@ -0,0 +1,21 @@
+// program to reverse the string
+function removeSpaces() {
+
+    // Take input from the user
+    const str = (document.getElementById("textInput").value.toString());
+    console.log(`Input text is : ${str}`);
+
+
+    // The split(' ') method is used to split the strings into individual array elements.
+    // The join('') method merges the array into a string.
+
+    const newStr = str.split(' ').join("");
+    console.log(newStr);
+
+
+    // const newStr = wordsArr
+    // console.log(newStr);
+
+
+    document.querySelector('#h4').innerHTML = newStr;
+}
